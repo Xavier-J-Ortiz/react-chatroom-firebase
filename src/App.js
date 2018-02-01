dcf042ebf0cfe7ec97cbd2fc1cf9d98aa5e494c5
xortiz <xavier.ortiz.ch@gmail.com>
2018-01-31 19:41:31 -0500

    added firebase information and other sorts.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyC6QH2Torq9EviDljQ5m9DKxYjP3mE_NCs",
+  authDomain: "react-chatroom-database.firebaseapp.com",
+  databaseURL: "https://react-chatroom-database.firebaseio.com",
+  projectId: "react-chatroom-database",
+  storageBucket: "react-chatroom-database.appspot.com",
+  messagingSenderId: "747996608929"
+};
+
+firebase.initializeApp(config);
 
 class App extends Component {
   render() {
