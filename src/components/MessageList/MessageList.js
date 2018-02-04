e6772a3ade738ebecdd43146bd953418e48d7d60
xortiz <xavier.ortiz.ch@gmail.com>
2018-02-03 23:54:07 -0500

    figured out how get the full amount of messages. However, I have to click a couple of times, and also the amount of messages sometimes I get are double.

--- src/components/MessageList/MessageList.js
+++ src/components/MessageList/MessageList.js
@@ -11,6 +11,36 @@ class MessageList extends Component {
   }
 
   componentDidMount() {
+    var query = this.messageRef.orderByChild('roomId').equalTo(this.state.currentRoom);
+    query.on('child_added', snapshot => {
+      const message = snapshot.val();
+      message.key = snapshot.key;
+      this.setState({
+        messages: this.state.messages.concat(message),
+        new_message: '',
+        query: query
+      })
+    });
+  }
+
+  componentWillReceiveProps(nextProp) {
+    this.setState({ 
+      currentRoom: nextProp.currentRoom,
+      messages: []
+    });
+    this.state.query.off('child_added');
+
+    var query = this.messageRef.orderByChild('roomId').equalTo(nextProp.currentRoom);
+    query.on('child_added', snapshot => {
+      const message = snapshot.val();
+      message.key = snapshot.key;
+      this.setState({
+        messages: this.state.messages.concat(message),
+        new_message: '',
+        query: query
+      })
+    });
+
     this.messageRef.orderByChild('roomId').equalTo(this.state.currentRoom).on('child_added', snapshot => {
       const message = snapshot.val();
       message.key = snapshot.key;
@@ -19,14 +49,11 @@ class MessageList extends Component {
         new_message: ''
       })
     });
-  }
 
-  componentWillReceiveProps(nextProp) {
-    this.setState({ currentRoom: nextProp.currentRoom });
   }
 
   render() {
-    console.log(this.state.currentRoom);
+    //console.log(this.state.currentRoom);
     //console.log(this.state.messages.map( (value, index) => "index: " + index + " message: " + value.content ));
     let formatted = this.state.messages.map( (value, index) => <li key={index}> {value.name}  :  {value.content} </li>)
     return (
